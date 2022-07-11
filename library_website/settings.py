92cca2a5b5a1d635af03bb2e68bbb454b9ed1cb2
TomTom2k <you@example.com>
2022-07-12 01:32:21 +0700

    deploy

--- library_website/settings.py
+++ library_website/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-euc+l@6)gs%9@3!j5urt=6x+w_0ypbw%7)b#my_)489)%ma_-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['library-website-api.herokuapp.com/',
+                 '127.0.0.1']
 
 
 # Application definition
