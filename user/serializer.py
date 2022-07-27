329f9ec2442c5d8bbd4efc22cde8702e26b318d2
TomTom2k <you@example.com>
2022-07-27 18:36:48 +0700

    is_author

--- user/serializer.py
+++ user/serializer.py
@@ -5,4 +5,4 @@ from user.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email", "password", "is_superuser", "is_staff"]
+        fields = ["username", "email", "password", "is_superuser", "is_author"]
