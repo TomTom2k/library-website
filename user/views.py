de9b62df59f724cf1cf376fdf18f8306cef1175f
TomTom2k <you@example.com>
2022-07-28 01:17:01 +0700

    abc xyz

--- user/views.py
+++ user/views.py
@@ -14,6 +14,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         # Add custom claims
         token['name'] = user.username
+        token['is_author'] = user.is_author
         # ...
 
         return token
